347d6038183996cd2e89d87d3e02a04040c7dd93
Michael Boyles <michael@boyl.es>
2022-10-25 22:50:38 +0100

    test: added for kotlin generation

---
+++ src/it/kotlinTest/src/test/java/com/github/michaelboyles/dgs/it/KotlinTest.java
@@ -0,0 +1,25 @@
+package com.github.michaelboyles.dgs.it;
+
+import org.junit.Test;
+
+import com.github.michaeboyles.dgs.generated.DgsConstants;
+import com.github.michaeboyles.dgs.generated.sub.Person;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class KotlinTest {
+    @Test
+    public void constantName() {
+        assertEquals("Person", DgsConstants.PERSON.TYPE_NAME);
+    }
+
+    @Test
+    public void builder() {
+        Person person = new Person("Michael", 28, true);
+
+        assertEquals("Michael", person.getName());
+        assertEquals(Integer.valueOf(28), person.getAge());
+        assertTrue(person.getLikesDogs());
+    }
+}
