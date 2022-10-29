7b3c843ae501bd682b3c10f5b80f004d61bb1d42
Michael Boyles <michael@boyl.es>
2022-10-29 20:18:43 +0100

    refactor: remove duplication between query and mutation generation

--- src/main/java/com/github/michaeboyles/dgs/java/JavaInterfaces.java
+++ src/main/java/com/github/michaeboyles/dgs/java/JavaInterfaces.java
@@ -2,14 +2,23 @@ package com.github.michaeboyles.dgs.java;
 
 import com.github.michaeboyles.dgs.GqlUtil;
 import com.github.michaeboyles.dgs.Packages;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
 import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeSpec;
 import graphql.language.Document;
+import graphql.language.FieldDefinition;
+import graphql.language.InputValueDefinition;
 import graphql.language.ObjectTypeDefinition;
 
+import javax.lang.model.element.Modifier;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.github.michaeboyles.dgs.java.TypeUtil.convertType;
 import static java.util.Collections.emptyList;
 
 public class JavaInterfaces {
@@ -26,13 +35,47 @@ public class JavaInterfaces {
 
     private static List<JavaFile> generateQueryInterfaces(Packages packages, ObjectTypeDefinition queryDef) {
         return queryDef.getFieldDefinitions().stream()
-            .map(def -> QueryInterface.generate(packages, def))
+            .map(def -> generate(packages, def, DgsQuery.class, "Query"))
             .collect(Collectors.toList());
     }
 
     private static List<JavaFile> generateMutationInterfaces(Packages packages, ObjectTypeDefinition queryDef) {
         return queryDef.getFieldDefinitions().stream()
-            .map(def -> MutationInterface.generate(packages, def))
+            .map(def -> generate(packages, def, DgsMutation.class, "Mutation"))
             .collect(Collectors.toList());
+    }
+
+    private static JavaFile generate(Packages packages, FieldDefinition query, Class<?> annotation, String suffix) {
+        return JavaFile.builder(
+            packages.interfacePackage(),
+            TypeSpec.interfaceBuilder(getClassName(query, suffix))
+                .addModifiers(Modifier.PUBLIC)
+                .addMethod(getMutationMethod(packages, query, annotation))
+                .build()
+        ).build();
+    }
+
+    private static String getClassName(FieldDefinition query, String suffix) {
+        return Character.toUpperCase(query.getName().charAt(0)) + query.getName().substring(1) + suffix;
+    }
+
+    private static MethodSpec getMutationMethod(Packages packages, FieldDefinition query, Class<?> annotation) {
+        return MethodSpec.methodBuilder(query.getName())
+            .addAnnotation(annotation)
+            .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+            .returns(convertType(packages, query.getType()))
+            .addParameters(getParameters(packages, query))
+            .build();
+    }
+
+    private static List<ParameterSpec> getParameters(Packages packages, FieldDefinition query) {
+        return query.getInputValueDefinitions().stream()
+            .map(def -> getParameter(packages, def))
+            .collect(Collectors.toList());
+    }
+
+    private static ParameterSpec getParameter(Packages packages, InputValueDefinition valueDefinition) {
+        return ParameterSpec.builder(convertType(packages, valueDefinition.getType()), valueDefinition.getName())
+            .build();
     }
 }
--- src/main/java/com/github/michaeboyles/dgs/kotlin/KotlinInterfaces.java
+++ src/main/java/com/github/michaeboyles/dgs/kotlin/KotlinInterfaces.java
@@ -2,14 +2,23 @@ package com.github.michaeboyles.dgs.kotlin;
 
 import com.github.michaeboyles.dgs.GqlUtil;
 import com.github.michaeboyles.dgs.Packages;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
 import com.squareup.kotlinpoet.FileSpec;
+import com.squareup.kotlinpoet.FunSpec;
+import com.squareup.kotlinpoet.KModifier;
+import com.squareup.kotlinpoet.ParameterSpec;
+import com.squareup.kotlinpoet.TypeSpec;
 import graphql.language.Document;
+import graphql.language.FieldDefinition;
+import graphql.language.InputValueDefinition;
 import graphql.language.ObjectTypeDefinition;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.github.michaeboyles.dgs.kotlin.TypeUtil.convertType;
 import static java.util.Collections.emptyList;
 
 public class KotlinInterfaces {
@@ -26,13 +35,48 @@ public class KotlinInterfaces {
 
     private static List<FileSpec> generateQueryInterfaces(Packages packages, ObjectTypeDefinition queryDef) {
         return queryDef.getFieldDefinitions().stream()
-            .map(def -> QueryInterface.generate(packages, def))
+            .map(def -> generate(packages, def, DgsQuery.class, "Query"))
             .collect(Collectors.toList());
     }
 
     private static List<FileSpec> generateMutationInterfaces(Packages packages, ObjectTypeDefinition queryDef) {
         return queryDef.getFieldDefinitions().stream()
-            .map(def -> MutationInterface.generate(packages, def))
+            .map(def -> generate(packages, def, DgsMutation.class, "Mutation"))
             .collect(Collectors.toList());
+    }
+
+    public static FileSpec generate(Packages packages, FieldDefinition queryDef, Class<?> annotation, String suffix) {
+        String className = getClassName(queryDef, suffix);
+        return FileSpec.builder(packages.interfacePackage(), className)
+            .addType(getInterfaceType(packages, queryDef, annotation, suffix))
+            .build();
+    }
+
+    private static String getClassName(FieldDefinition query, String suffix) {
+        return Character.toUpperCase(query.getName().charAt(0)) + query.getName().substring(1) + suffix;
+    }
+
+    private static TypeSpec getInterfaceType(Packages packages, FieldDefinition query, Class<?> annotation, String suffix) {
+        return TypeSpec.interfaceBuilder(getClassName(query, suffix))
+            .addFunction(
+                FunSpec.builder(query.getName())
+                    .addAnnotation(annotation)
+                    .addModifiers(KModifier.PUBLIC, KModifier.ABSTRACT)
+                    .addParameters(getParameters(packages, query))
+                    .returns(convertType(packages, query.getType()))
+                    .build()
+            )
+            .build();
+    }
+
+    private static List<ParameterSpec> getParameters(Packages packages, FieldDefinition query) {
+        return query.getInputValueDefinitions().stream()
+            .map(def -> getParameter(packages, def))
+            .collect(Collectors.toList());
+    }
+
+    private static ParameterSpec getParameter(Packages packages, InputValueDefinition valueDefinition) {
+        return ParameterSpec.builder(valueDefinition.getName(), convertType(packages, valueDefinition.getType()))
+            .build();
     }
 }
